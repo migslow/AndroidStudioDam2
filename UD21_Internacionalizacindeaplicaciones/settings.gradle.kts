bec61caada1bfe0fefac909a74ab0367a49b6f19
DAM2Alu10 <miovejero.pi@gmail.com>
2023-09-21 10:26:44 +0200

    Comienzo layouts

---
+++ UD21_Internacionalizacindeaplicaciones/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UD2-1_Internacionalización de aplicaciones"
+include(":app")
+ 
