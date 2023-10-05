2e93684836f7d15de373f2b34d0fa0fb40990cfe
DAM2Alu10 <miovejero.pi@gmail.com>
2023-10-05 09:21:15 +0200

    Calculadora IMC no completada

---
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.calculadoraimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static Button b;
+    static TextView tv1, tv2;
+    static EditText et1;
+    static EditText et2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.buttonCalcular);
+        tv1 = findViewById(R.id.textViewPeso);
+        tv2 = findViewById(R.id.textViewEstatura);
+        et1 = findViewById(R.id.editTextTextPeso);
+        et2 = findViewById(R.id.editTextTextEstatura);
+    }
+
+    public void clic (View view){
+        Intent i = new Intent(this, MainActivity2.class);
+
+        startActivity(i);
+    }
+
+    public static double calculoIMC(double peso, int estatura){
+        
+    }
+
+}
