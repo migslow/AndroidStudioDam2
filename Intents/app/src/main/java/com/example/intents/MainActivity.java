55bb240850de2ebdae83b90e0555a088b3e2b7ee
DAM2Alu10 <miovejero.pi@gmail.com>
2023-10-05 09:00:27 +0200

    Intent 1

---
+++ Intents/app/src/main/java/com/example/intents/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.button1);
+    }
+
+    public void clic (View v){
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
+
+
+}
